96e61777007ebac7653814a4f629717c482db5ce
Nabelanita <57351716+nabelanita@users.noreply.github.com>
2023-09-18 18:30:03 +0700

    feat(add data mobile styling): add mobile styling for add data page
    fixed form column sizes, fix interactivity for mobile.

--- src/pages/AddData.js
+++ src/pages/AddData.js
@@ -93,7 +93,102 @@ function AddData() {
       <Container>
         <h3 className='af-title'>Tambah Data Baru</h3>
         <div className='af-container d-flex justify-content-center'>
-          <form onSubmit={handleSubmit} className='af-form'>
+        <form onSubmit={handleSubmit} className='af-form row'>
+          <div className='af-elmt'>
+            <label htmlFor="komoditas" className='af-label col-12'>Komoditas</label>
+            <br />
+            <input
+              className='af-text col-12'
+              type="text"
+              id="komoditas"
+              name="komoditas"
+              value={formData.komoditas}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className='col-12'>
+            <div className='row'>
+              <div className='af-elmt col-12 col-md-6 col-lg-12'>
+                <label htmlFor="province" className='af-label col-12'>Provinsi</label>
+                <br />
+                <select
+                  className='af-select col-12'
+                  id="province"
+                  name="province"
+                  value={formData.province}
+                  onChange={handleProvinsiChange}
+                  required
+                >
+                  <option value="">Select Provinsi</option>
+                  {provinsiOptions.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+                </select>
+              </div>
+              <div className='af-elmt col-12 col-md-6 col-lg-12'>
+                <label htmlFor="city" className='af-label col-12'>Kota</label>
+                <br />
+                <select
+                  id="city"
+                  name="city"
+                  value={formData.city}
+                  onChange={handleInputChange}
+                  className='af-select col-12'
+                  required
+                >
+                  <option value="">Select Kota</option>
+                  {kotaOptions[selectedProvince]?.map((city) => (
+                  <option key={city} value={city}>
+                    {city}
+                  </option>
+                ))}
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className='col-12'>
+            <div className='row'>
+              <div className='af-elmt col-5 col-md-4 col-lg-4'>
+                <label htmlFor="size" className='af-label col-12'>Size</label>
+                <br />
+                <select
+                  id="size"
+                  name="size"
+                  value={formData.size}
+                  onChange={handleInputChange}
+                  className='af-select'
+                  required
+                >
+                  <option value="">Select Size</option>
+                  {sizeOptions.map((size) => (
+                    <option key={size} value={size}>
+                      {size}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className='af-elmt col-7 col-md-8 col-lg-8'>
+                <label htmlFor="price" className='af-label col-12'>Harga</label>
+                <br />
+                <input
+                  className='af-text col-12'
+                  type="number"
+                  id="price"
+                  name="price"
+                  value={formData.price}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+            </div>
+
+          </div>
+            <button type="submit" className='btn af-submit col-12'>Submit</button>
+          </form>
+          {/* <form onSubmit={handleSubmit} className='af-form'>
             <div className='af-elmt'>
               <label htmlFor="komoditas" className='af-label'>Komoditas</label>
               <br />
@@ -178,7 +273,7 @@ function AddData() {
               />
             </div>
             <button type="submit" className='btn af-submit'>Submit</button>
-          </form>
+          </form> */}
         </div>
       </Container>
     </div>
